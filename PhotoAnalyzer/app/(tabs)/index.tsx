ef7e17751525b7149207079f8a8bdd8adad67f14
Lahon <lahonbhargab@gmail.com>
2025-07-29 01:25:14 +0530

    first commit

---
+++ PhotoAnalyzer/app/(tabs)/index.tsx
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+import App from '../../../Photo1/App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+registerRootComponent(App);
